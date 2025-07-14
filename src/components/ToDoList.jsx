b1d8c82706f1def96b3ebb151a69fb8f8a0d38d1
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2025-07-14 10:03:45 +0200

    added a to-do tag

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -3,10 +3,14 @@ export default function ToDoList() {
     return (
         <>
             <div className="flex flex-col justify-center items-center mt-4">
-                <p>Type something</p>
+                <p className="font-600 font-bold text-white">TYPE SOMETHING</p>
                 <div className="mt-4">
-                    <input type="text" className=" border-cyan-100 border-1 outline-none h-8 w-150"/>
-                    <button className="w-18 h-8 bg-cyan-100 cursor-pointer">Add</button>
+                    <input type="text" className="bg-white text-black border-cyan-100 border-1 outline-none h-8 w-150"/>
+                    <button className="w-18 h-8 text-white bg-purple-600 cursor-pointer">Add</button>
+                </div>
+                <div className="mt-4 flex items-center justify-between bg-gray-100 text-black w-168 h-20 p-4">
+                    <p></p>
+                    <button className=" w-18 h-8 text-white bg-blue-600 cursor-pointer">Delete</button>
                 </div>
             </div>
         </>
